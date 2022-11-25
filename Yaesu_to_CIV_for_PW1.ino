a963ce89cf158e02d97b0d5d6ad10bab9c85f38e
René <37976634+dj1yr@users.noreply.github.com>
2022-11-25 13:23:22 +0100

    Add files via upload

--- Yaesu_to_CIV_for_PW1.ino
+++ Yaesu_to_CIV_for_PW1.ino
@@ -1,5 +1,10 @@
 
 
+//#define uart Serial1
+#define uart Serial
+
+
+
 int y_a = 2;
 int y_b = 3;
 int y_c = 4;
@@ -32,7 +37,7 @@ boolean start_loop =true;
 
 
 void setup(){
-Serial1.begin(9600);
+uart.begin(9600);
 
 
 
@@ -65,18 +70,18 @@ tx_in = digitalRead(6);
 
 // Yaesu bcd to CIV frequency ->Mhz+100*khz
 
-if (y_a==0 && y_b==0 && y_c==0 && y_d==0){bnd_2=0x51; bnd_3=0x00; band=1;}    //no band signals -> select 6m send 51.00Mhz
-if (y_a==1 && y_b==0 && y_c==0 && y_d==0){bnd_2=0x01; bnd_3=0x90; band=2;}    //160m send 1.90Mhz
-if (y_a==0 && y_b==1 && y_c==0 && y_d==0){bnd_2=0x03; bnd_3=0x70; band=3;}    //80m send 3.70Mhz
-if (y_a==1 && y_b==1 && y_c==0 && y_d==0){bnd_2=0x07; bnd_3=0x10; band=4;}    //60m/40m send 7.10Mhz
-if (y_a==0 && y_b==0 && y_c==1 && y_d==0){bnd_2=0x10; bnd_3=0x05; band=5;}    //30m send 10.05Mhz
-if (y_a==1 && y_b==0 && y_c==1 && y_d==0){bnd_2=0x14; bnd_3=0x20; band=6;}    //20m send 14.20Mhz
-if (y_a==0 && y_b==1 && y_c==1 && y_d==0){bnd_2=0x18; bnd_3=0x06; band=7;}    //17m send 18.06Mhz
-if (y_a==1 && y_b==1 && y_c==1 && y_d==0){bnd_2=0x21; bnd_3=0x20; band=8;}    //15m send 21.20Mhz
-if (y_a==0 && y_b==0 && y_c==0 && y_d==1){bnd_2=0x24; bnd_3=0x90; band=9;}    //12m send 24.90Mhz
-if (y_a==1 && y_b==0 && y_c==0 && y_d==1){bnd_2=0x28; bnd_3=0x50; band=10;}    //10m send 25.500Mhz
-if (y_a==0 && y_b==1 && y_c==0 && y_d==1){bnd_2=0x51; bnd_3=0x00; band=11;}    //6m send 51.00Mhz
-if (y_a==1 && y_b==1 && y_c==1 && y_d==1){bnd_2=0x51; bnd_3=0x00; band=12;}    // Yaesu 4m band selected, send 51.00Mhz
+if (y_a==0 && y_b==0 && y_c==0 && y_d==0){bnd_2=0x51; bnd_3=0x00; tx_ok = false; band=1;}    //no band signals -> select 6m send 51.00Mhz
+if (y_a==1 && y_b==0 && y_c==0 && y_d==0){bnd_2=0x01; bnd_3=0x90; tx_ok = true; band=2;}    //160m send 1.90Mhz
+if (y_a==0 && y_b==1 && y_c==0 && y_d==0){bnd_2=0x03; bnd_3=0x70; tx_ok = true; band=3;}    //80m send 3.70Mhz
+if (y_a==1 && y_b==1 && y_c==0 && y_d==0){bnd_2=0x07; bnd_3=0x10; tx_ok = true; band=4;}    //60m/40m send 7.10Mhz
+if (y_a==0 && y_b==0 && y_c==1 && y_d==0){bnd_2=0x10; bnd_3=0x05; tx_ok = true; band=5;}    //30m send 10.05Mhz
+if (y_a==1 && y_b==0 && y_c==1 && y_d==0){bnd_2=0x14; bnd_3=0x20; tx_ok = true; band=6;}    //20m send 14.20Mhz
+if (y_a==0 && y_b==1 && y_c==1 && y_d==0){bnd_2=0x18; bnd_3=0x06; tx_ok = true; band=7;}    //17m send 18.06Mhz
+if (y_a==1 && y_b==1 && y_c==1 && y_d==0){bnd_2=0x21; bnd_3=0x20; tx_ok = true; band=8;}    //15m send 21.20Mhz
+if (y_a==0 && y_b==0 && y_c==0 && y_d==1){bnd_2=0x24; bnd_3=0x90; tx_ok = true; band=9;}    //12m send 24.90Mhz
+if (y_a==1 && y_b==0 && y_c==0 && y_d==1){bnd_2=0x28; bnd_3=0x50; tx_ok = true; band=10;}    //10m send 25.500Mhz
+if (y_a==0 && y_b==1 && y_c==0 && y_d==1){bnd_2=0x51; bnd_3=0x00; tx_ok = true; band=11;}    //6m send 51.00Mhz
+if (y_a==1 && y_b==1 && y_c==1 && y_d==1){bnd_2=0x51; bnd_3=0x00; tx_ok = false; band=12;}    // Yaesu 4m band selected, send 51.00Mhz
 
 
 
@@ -88,7 +93,7 @@ if (y_a==1 && y_b==1 && y_c==1 && y_d==1){bnd_2=0x51; bnd_3=0x00; band=12;}    /
 
     if (uart_busy==false){
       uart_busy = true;
-      daten_pw1 = Serial1.read();
+      daten_pw1 = uart.read();
 
         if ((daten_pw1 == 254)){ 
             PA_data_start();
@@ -98,11 +103,10 @@ if (y_a==1 && y_b==1 && y_c==1 && y_d==1){bnd_2=0x51; bnd_3=0x00; band=12;}    /
 
     }
 
-    if(tx_in == LOW){
+    if(tx_in == LOW && tx_ok == true){
       uart_busy = true;
       data_send_1();
       digitalWrite(tx_out,HIGH);
-
     }
     else{
       digitalWrite(tx_out,LOW);
@@ -119,9 +123,9 @@ if (y_a==1 && y_b==1 && y_c==1 && y_d==1){bnd_2=0x51; bnd_3=0x00; band=12;}    /
 delay(10);
   for ( i = 0; i < 6; i++) {                     
 
-    if (Serial1.available() > 0) {           
+    if (uart.available() > 0) {           
 
-      buffget_pw1tx[i] = Serial1.read();     
+      buffget_pw1tx[i] = uart.read();     
 
       String pw1_string = String(buffget_pw1tx[i], HEX);
 
@@ -139,32 +143,32 @@ delay(10);
  }
  void data_send_1(){
    uart_busy = true;
-        Serial1.write(data_start,2);    //0xFE start of telegram, send 2x 0xFE
-        Serial1.write(to_addr);      //PW1 adress
-        Serial1.write(from_addr);    //TRX adress
-        Serial1.write(cmd_1);    //10hz+1hz
-        Serial1.write(bnd_5);    //1khz+100hz    
-        Serial1.write(bnd_4);    //10khz
-        Serial1.write(bnd_3);    //100khz
-        Serial1.write(bnd_2);    //10Mhz
-        Serial1.write(bnd_1);    //100Mhz
-        Serial1.write(data_stop);    //0xFD end of telegramm
+        uart.write(data_start,2);    //0xFE start of telegram, send 2x 0xFE
+        uart.write(to_addr);      //PW1 adress
+        uart.write(from_addr);    //TRX adress
+        uart.write(cmd_1);    //10hz+1hz
+        uart.write(bnd_5);    //1khz+100hz    
+        uart.write(bnd_4);    //10khz
+        uart.write(bnd_3);    //100khz
+        uart.write(bnd_2);    //10Mhz
+        uart.write(bnd_1);    //100Mhz
+        uart.write(data_stop);    //0xFD end of telegramm
   band_old=band;
   uart_busy = false;
  }
 
   void data_send_2(){
   uart_busy = true;
-        Serial1.write(data_start,2);    //0xFE start of telegram, send 2x 0xFE
-        Serial1.write(to_addr);      //PW1 adress
-        Serial1.write(from_addr);    //TRX adress
-        Serial1.write(cmd_2);    
-        Serial1.write(bnd_5);    //1khz+100hz    
-        Serial1.write(bnd_4);    //10khz
-        Serial1.write(bnd_3);    //100khz
-        Serial1.write(bnd_2);    //10Mhz
-        Serial1.write(bnd_1);    //100Mhz
-        Serial1.write(data_stop);    //0xFD end of telegramm
+        uart.write(data_start,2);    //0xFE start of telegram, send 2x 0xFE
+        uart.write(to_addr);      //PW1 adress
+        uart.write(from_addr);    //TRX adress
+        uart.write(cmd_2);    
+        uart.write(bnd_5);    //1khz+100hz    
+        uart.write(bnd_4);    //10khz
+        uart.write(bnd_3);    //100khz
+        uart.write(bnd_2);    //10Mhz
+        uart.write(bnd_1);    //100Mhz
+        uart.write(data_stop);    //0xFD end of telegramm
         start_loop = false;
         uart_busy = false;
   }
@@ -172,13 +176,13 @@ delay(10);
  
   void data_send_3(){
   uart_busy = true;
-        Serial1.write(data_start,2);    //0xFE start of telegram, send 2x 0xFE
-        Serial1.write(to_addr);      //PW1 adress
-        Serial1.write(from_addr);    //TRX adress
-        Serial1.write(cmd_3);    //10hz+1hz
-        Serial1.write(0x00);
-        Serial1.write(mode);
-        Serial1.write(data_stop);    //0xFD end of telegramm
+        uart.write(data_start,2);    //0xFE start of telegram, send 2x 0xFE
+        uart.write(to_addr);      //PW1 adress
+        uart.write(from_addr);    //TRX adress
+        uart.write(cmd_3);    //10hz+1hz
+        uart.write(0x00);
+        uart.write(mode);
+        uart.write(data_stop);    //0xFD end of telegramm
   uart_busy = false;
  }
  
